8f7833a06543a7b241ede061e62ad3069d1826db
Alex Jang <alexander_jang@brown.edu>
2019-04-10 15:51:35 -0400

    translation/detection implementation

---
+++ src/nlp/translation/languageDetection.js
@@ -0,0 +1,13 @@
+const {TranslationServiceClient} = require('@google-cloud/translate').v3beta1;
+const apiKey = require('./googleTranslateAPIKey');
+
+const translationClient = new TranslationServiceClient();
+
+function detectLanguage(text) {
+	const request = {
+		parent: translationClient.locationPath(apiKey.projectId, 'global'),
+		contents: text
+	};
+
+	return translationClient.detectLanguage(request);
+}
