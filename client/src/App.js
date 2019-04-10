6bc6d7a5794b980813deb9010e4a5f25be93f86d
Jacob Meltzer <jacobmeltzer@jacobs-mbp-2.devices.brown.edu>
2019-04-10 16:10:40 -0400

    Basic routes and 2 components

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
 
-class App extends Component {
+import Home from "./Home";
+import Navbar from "./Navbar";
+
+class App extends React.Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/create' component={Home} />
+            <Route path='/login' component={Home} />
+            <Route path='/signup' component={Home} />
+            <Route path='/settings' component={Home} />
+            <Route path='/:id/sheets' component={Home} />
+            <Route path='/display' component={Home} />
+
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
---
+++ client/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    render() {
+      return (
+        <div>
+            <ul>
+                <li><a href="/login">Login</a></li>
+            </ul>
+        </div>
+      )
+    }
+  }
+
+  export default Navbar;
