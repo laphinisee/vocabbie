09279fd8a33e54a3ebac1fedf7a5b7d5ae8cc5d7
Jacob Meltzer <jacobmeltzer@jacobs-mbp-2.devices.brown.edu>
2019-04-12 16:28:04 -0400

    basic login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Container from './Container';
+import {Form, FormField, Button} from 'grommet';
 
 class Login extends React.Component {
 
     render() {
       return (
         <Container title="Login" description="Login!">
-            <p>login content goes here</p>
+            <Form>
+                <FormField name="email" type="email" label="Email" />
+                <FormField name="password" type="password" label="Password" />
+                <Button type="submit" primary label="Login" />
+            </Form>
         </Container>
       )
     }
