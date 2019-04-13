f58ad210c9be12f34d0c56b21b23c15e7375f20c
Jacob Meltzer <jacobmeltzer@jacobs-mbp-2.devices.brown.edu>
2019-04-13 15:38:01 -0400

    Homepage

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,4 +1,29 @@
 import React from "react";
+import {Heading, Box, Button, Grid} from 'grommet';
+import map from './images/map.png';
+
+const Header = (props) => (
+  <Box
+      className='PageHeader'
+      direction='column'
+      align='center'
+      justify='center'
+      background={`url(${map})`}
+      pad={{ left: 'medium', right: 'medium', vertical: 'large' }}>
+      <Heading color='white'>Build language fluency with Vocabbie. </Heading>
+      <Grid
+        rows={['fill']}
+        columns={['fill']}
+        gap="small"
+        areas={[
+          { name: 'one', start: [0, 0], end: [0, 0] },
+          { name: 'two', start: [1, 0], end: [1, 0] },
+        ]}>
+        <Box gridArea='one'><Button primary={true} label="Get Started" /></Box>
+        <Box grideArea='two'><Button primary={true} label="Keep Going" /></Box>
+      </Grid>
+   </Box>
+)
 
 class Home extends React.Component {
 
@@ -9,7 +34,7 @@ class Home extends React.Component {
 
     render() {
       return (
-        <p>welcome to the homepage</p>
+        <Header/>
       )
     }
   }
