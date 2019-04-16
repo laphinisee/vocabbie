495cf0cb347b14321fccbbd3ef30349e6938620b
Alex Jang <alexander_jang@brown.edu>
2019-04-16 17:25:50 -0400

    query.js master file

---
+++ src/db/utils/query.js
@@ -0,0 +1,5 @@
+const studyMatQueries = require('./queries/studyMatQueries');
+const userQueries = require('./queries/userQueries');
+
+module.exports.studyMat = studyMatQueries;
+module.exports.user = userQueries;
