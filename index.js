3e0d3dbd9605b0673f2bca76d7d7624f058a6829
Lisa Phinisee <laphinisee@gmail.com>
2019-04-21 14:15:24 -0400

    wrote userid/vocab entry point

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const nlp = require('src/nlp')
+const querydb = require('src/db/query.js');
 ////////////////////// End boilerplate //////////////////////
 
 
@@ -69,32 +70,25 @@ app.post('/generate-text', function(request, response) {
   	}
   });
 
-  const newDocumentText = {
-    plaintext : request.body.text,
-    sourceLanguage : srclanguage,
-    targetLanguage : "en",
-    allWords : allWords,
-    keyWords : keywords
-  }
-  // get user information from session token
-  const newDocument = {
-  	text: newDocumentText,
-  	name : ,
-  	owner : ,
-    sharedUsers : ,
-    StudyMats: {}
-  };
   // call db function to save all words.
+  querydb.createDocument(/*name*/ "", /*ownerId*/ "", request.body.text, srcLanguage, "en", allWords, keywords);
   response.status(200).send();
 });
 
 app.post('/:userid/vocab', function(request, response){
-
-	const userdid = request.params.userid;
-	let vocabToSave = response.body.vocabToSave;
-	// call db function to get a list of documents (title + preview) associated with this user 
+	const titles = [];
+	const ids = [];
+	const previews = [];
+	querydb.getUserDocuments(request.params.userid)
+	.then(result => (){
+		// list of {name : ?, _id : ?, text.plaintext : ?}
+		titles.push(result.name);
+		ids.push(result._id);
+		let len = result.text.plaintext.length > 100 ? 100 : result.text.plaintext.length;
+		previews.push(result.text.plaintext.substring(0, len));
+	});
 	response.status(200).type('html');
-	response.json(toReturn);
+	response.json({titles : titles, ids : ids, previews : previews});
 });
 
 app.get('/*/settings', function(request, response){
