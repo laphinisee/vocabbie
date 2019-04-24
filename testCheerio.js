9117b47f137d2c8f62a766deb8106c55ec81e6d0
alex-jang <alexander_jang@brown.edu>
2019-04-24 17:35:17 -0400

    Update testCheerio.js

--- testCheerio.js
+++ testCheerio.js
@@ -1,9 +1,10 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
 
+const textElements = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a'];
 axios.get('https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States').then((response) => {
   // Load the web page source code into a cheerio instance
   const $ = cheerio.load(response.data);
-  const allText = $('p').text() + " " + $('h1').text() + " " + $('h2').text() + " " + $('h3').text() + " " + $('h4').text() + " " +$('h5').text() + " " + $('h6').text();
+  const allText = textElements.map(element => $(element).text()).join(' ');
   return 
 });
